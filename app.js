4529cf38b386c144a71d4c47fd1acb2acf18ef0e
Dhanshree Handge <115164271+Dhanshree2609@users.noreply.github.com>
2024-03-06 22:35:00 +0530

    song added

---
+++ app.js
@@ -0,0 +1,13 @@
+const music = new Audio('songs/Attention.mp3');
+// music.play();
+
+const songs =[
+    {
+        id: '1',
+        songName:`<ul style="list-style-type: none;">
+        <li class="dark">Attention</li>
+        <li class="light">Charlie Puth</li>
+        </ul>`,
+        poster : "images/song (1).png"
+    }
+]
