9060228ad8a65928a20c270d608da0c065e45979
Dhanshree Handge <115164271+Dhanshree2609@users.noreply.github.com>
2024-03-14 12:51:35 +0530

    Correction

--- login.php
+++ login.php
@@ -1,20 +1,20 @@
 <?php
 
-// Retrieve values from the login form
-$username = $_POST['username'];
-$password = $_POST['password'];
+  // Retrieve values from the login form
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-$conn = new mysqli('localhost', 'root', '', 'login');
+  $conn = new mysqli('localhost', 'root', '', 'login');
 
-if ($conn->connect_error) {
+  if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
-else{
+  }
+  else{
     $stmt=$conn->prepare("insert into registration(username,password) values(?,?)");
     $stmt->bind_param("ss",$username,$password);
     $stmt->execute();
     echo"Logged In sucessfully.....";
     $stmt->close();
     $conn->close();
-}
+  }
 ?>
